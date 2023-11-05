0c567bb911c2266dab3a1cfa7332e2028c9e2aaf
Kosuke-Sugimoto <sk12852019@outlook.jp>
2023-11-06 03:38:30 +0900

    scripts for making environment

---
+++ env/docker_run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DOCKER_IMAGE_NAME=pytorch_training:v1
+DOCKER_CONTAINER_NAME=pt
+CURRENT_WORKING_DIR=$(pwd)/
+
+if [[ "$(docker images -q $DOCKER_IMAGE_NAME 2> /dev/null)" == ""  ]]
+then
+    docker build -t $DOCKER_IMAGE_NAME .
+fi
+
+docker run -it \
+           --name $DOCKER_CONTAINER_NAME \
+           --mount type=bind,src=$CURRENT_WORKING_DIR,dst=/work/ \
+           $DOCKER_IMAGE_NAME
