7044519b70fad71e70e3d8e01a32149001513d58
Kosuke-Sugimoto <sk12852019@outlook.jp>
2023-11-11 22:15:28 +0900

    docker_run.shがenvディレクトリより上で実行できるように修正

--- env/docker_run.sh
+++ env/docker_run.sh
@@ -6,7 +6,7 @@ CURRENT_WORKING_DIR=$(pwd)/
 
 if [[ "$(docker images -q $DOCKER_IMAGE_NAME 2> /dev/null)" == ""  ]]
 then
-    docker build -t $DOCKER_IMAGE_NAME .
+    docker build -t $DOCKER_IMAGE_NAME -f "${CURRENT_WORKING_DIR}env/Dockerfile" .
 fi
 
 docker run -it \
